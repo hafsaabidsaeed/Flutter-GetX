1f03d85fed713dc43447a368260ddf05e55e43e4
hafsaabidsaeed <128362965+hafsaabidsaeed@users.noreply.github.com>
2024-04-28 03:43:43 +0500

    Getx
    Snackbar using GetX in Flutter

--- getx/lib/home_screen.dart
+++ getx/lib/home_screen.dart
@@ -1,15 +1,33 @@
-import 'package:flutter/material.dart';
+//Snackbar in
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SnackBarScreen extends StatefulWidget {
+  const SnackBarScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<SnackBarScreen> createState() => _SnackBarScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _SnackBarScreenState extends State<SnackBarScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text( "Getx in flutter"),
+      ),
+      body: const Column(
+        children: [
+
+      ],),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.snackbar("Learn Getx", "Hello Hafsa Abid I want to learn about Getx in flutter")
+          SnackPosition: SnackPosition.BOTTOM;
+          // SnackPosition: SnackPosition.TOP;
+          
+      },),
+    );
   }
 }
